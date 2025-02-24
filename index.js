789f21ed458b6485e13501a35a9d221a837f81f7
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-02-24 14:00:04 -0500

    gotta fin

---
+++ index.js
@@ -0,0 +1,9 @@
+let storyLine = document.getElementById("currentStoryLine")
+let button1 = document.getElementById("B1")
+let button2 = document.getElementById("B2")
+let outCome = document.getElementById("outCome")
+
+
+
+
+// yes and no value of the main path 2 (Staying on the ship)
